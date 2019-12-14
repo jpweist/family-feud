15f2054678ee166377ef749c568e7441acf8bc68
Jim Weist <jp_weist@yahoo.com>
2019-12-14 12:10:21 -0700

    added test files and some tests for player class

---
+++ test/game-test.js
@@ -0,0 +1,9 @@
+import chai from 'chai';
+const expect = chai.expect;
+
+describe('Game Class', function() {
+  it('should return true', function() {
+    expect(true).to.equal(true);
+  });
+
+});
---
+++ test/turn-test.js
@@ -0,0 +1,8 @@
+import chai from 'chai';
+const expect = chai.expect;
+
+describe('Turn Class', function() {
+  it('should return true', function() {
+    expect(true).to.equal(true);
+  });
+});
