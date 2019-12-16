6373a64d0f5ab6fff9014902806654b31c933e7c
Cody Smith <codysmith2814@gmail.com>
2019-12-16 14:39:51 -0700

    Dynamically adds gameplay page

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,3 +1,38 @@
-// $( document ).ready({
-//   console.log("readdy jQuery");
-// });
+import $ from 'jquery';
+
+
+let instructionsPage = document.querySelector('.instructions-page');
+let startBtn = document.querySelector('.start-btn');
+startBtn.addEventListener('click', hideInstructions);
+
+function hideInstructions() {
+  instructionsPage.style.display = "none";
+  displayGamePage()
+}
+
+function displayGamePage() {
+  document.querySelector('.main-container').insertAdjacentHTML('afterbegin', `<section class="page-two-top"> 
+  <button class="info-button">i</button>
+  </section>
+  <section class="player-turn">
+    <h1 class="current-turn">It's Player 1's turn!</h1>
+    <h2 class="question">Name Something You Do To An Item Before Giving It As A Gift</h2>
+    <table class="answers">
+      <tr>
+        <td>Buy it</td><td>4</td>
+      </tr>
+      <tr>
+        <td>Remove Price tag</td><td>27</td>
+      </tr>
+      <tr>
+        <td>Wrap it</td><td>61</td>
+      </tr>
+    </table>
+Answers 
+     <section class="page-two-bottom"> 
+      <h3 class="answer">Enter Answer:</h3>
+      <input type="text" class="answer-input" placeholder="Type your answer" />
+      <button class="submit-button">Submit</button>
+    </section>`
+  )
+}
