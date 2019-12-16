698065eb044169de40bcdbe7d6fddea014484085
Cody Smith <codysmith2814@gmail.com>
2019-12-16 16:04:36 -0700

    Adds names from either player input

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,21 +1,23 @@
 import $ from 'jquery';
+// Variables
+let playerName = $('.player-name')
+let startBtn = $('.start-btn');
 
 
-let instructionsPage = document.querySelector('.instructions-page');
-let startBtn = document.querySelector('.start-btn');
-startBtn.addEventListener('click', hideInstructions);
-
-function hideInstructions() {
-  instructionsPage.style.display = "none";
+const hideInstructions = () => {
+  $('.instructions-page').toggleClass('hide-class');
   displayGamePage()
 }
 
-function displayGamePage() {
-  document.querySelector('.main-container').insertAdjacentHTML('afterbegin', `<section class="page-two-top"> 
+const displayGamePage = () => {
+  console.log(playerName)
+  document.querySelector('.main-container').insertAdjacentHTML('afterbegin', `
+  <section class="gameplay-page">
+  <section class="page-two-top"> 
   <button class="info-button">i</button>
   </section>
   <section class="player-turn">
-    <h1 class="current-turn">It's Player 1's turn!</h1>
+    <h1 class="current-turn">It's ${playerName[0].value || playerName[1].value}'s turn!</h1>
     <h2 class="question">Name Something You Do To An Item Before Giving It As A Gift</h2>
     <table class="answers">
       <tr>
@@ -33,6 +35,12 @@ Answers
       <h3 class="answer">Enter Answer:</h3>
       <input type="text" class="answer-input" placeholder="Type your answer" />
       <button class="submit-button">Submit</button>
+    </section>
     </section>`
   )
 }
+
+// Event Listeners
+$(startBtn).on('click', hideInstructions);
+
+
