a9308f939b50cd34616aa6679d2bfb1731a69978
Kim McCaskill <kimmccaskill92@gmail.com>
2019-12-18 19:28:56 -0700

    Remove html

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -8,42 +8,17 @@ import './index';
 import data from './data.js';
 
 const domUpdates = {
-  // Event Listeners
   openInfo() {
-    $(".gameplay-page").append(`
-      <section class="info-container">
-        <button class="close-btn">X</button>
-        <header class="plyr-info">
-          <div class="plyr-section">
-          <p>${player1.name}</p>
-          <p>Score: ${player1.score}</p>
-          </div>
-          <div class="plyr-section">
-            <p>${player2.name}</p>
-            <p>Score: ${player2.score}</p>
-          </div>
-        </header>
-        <div class="round-info">
-        <p>ROUND</p>
-        <p>1</p>
-        </div>
-        <button class="leaderboard-btn">Leaderboard</button>
-        <div class="new-quit-container">
-          <button class="new-game-btn">New Game</button>
-          <button class="quit-game-btn">Quit Game</button>
-        </div>
-      </section>`)
-  $(".close-btn").click(closeInfo);
+    $(".info-container").toggle(".hide-class")
   },
 
   closeInfo() {
-    $(".info-container").remove();
+    $(".info-container").toggle(".hide-class")
   },
 
   loadDOM() {
   $('.instructions-page').toggleClass('hide-class');
-  // game.findSurveys();
-  displayGamePage();
+  this.displayGamePage();
   },
 
   randomizeSurvey() {
@@ -57,37 +32,7 @@ const domUpdates = {
   },
 
   displayGamePage() {
-  console.log('hi', )
-  document.querySelector('.main-container').insertAdjacentHTML('afterbegin', `
-  <section class="gameplay-page">
-    <section class="gameplay-top">
-      <button class="info-btn">i</button>
-      <h1 class="current-turn">It's ${player1.name}'s turn!</h1>
-    </section>
-      <h2 class="question"></h2>
-      <div class="answers-container">
-        <table class="answers">
-          <tr>
-            <td>Buy it</td><td>4</td>
-          </tr>
-          <tr>
-            <td>Remove Price tag</td>
-            <td>27</td>
-          </tr>
-          <tr>
-            <td>Wrap it</td><td>61</td>
-          </tr>
-        </table>
-      </div>
-      <form label="Answer input" class="answer-form">
-        <h3 class="answer-label">Enter Answer</h3>
-        <input type="text" class="answer-input" placeholder="Type your answer" />
-        <button class="submit-btn">Submit</button>
-      </form>
-
-  </section>`
-  )
-  $(".info-btn").click(openInfo);
+    $(".gameplay-page").toggle(".hide-class")
   },
 
   checkInput() {
@@ -106,14 +51,11 @@ const domUpdates = {
   } if (playerName[0].value && playerName[1].value) {
     noError = true;
   } if (noError) {
-    loadDOM()
+    this.loadDOM()
   }
   },
 }
 
-
 const playerName = $('.plyr-input');
-// let randomNum = Math.floor(Math.random() * 15 + 1);
-let player1, player2;
 
 export default domUpdates;
