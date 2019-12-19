ee71dae8690c57731cc9681af5b17cd458e2301c
Kim McCaskill <kimmccaskill92@gmail.com>
2019-12-18 19:28:33 -0700

    Add event listeners for info btn

--- src/index.js
+++ src/index.js
@@ -4,32 +4,37 @@
 // An example of how you import jQuery into a JS file if you use jQuery in that file
 import $ from 'jquery';
 import domUpdates from './domUpdates.js'
-import Player from './player';
-import Turn from './turn';
-import Round from './round';
 import Game from './game';
-
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
 
+let game;
 
-
-let player1, player2, game;
-let apiDataAnswers;
-let apiDataSurveys;
-
-function getApiData() {
+const getApiData = () => {
   fetch("https://fe-apps.herokuapp.com/api/v1/gametime/1903/family-feud/data")
   .then(response => response.json())
-  .then(apiData => startGame(apiData.data))
+  .then(apiData =>
+    startGame(apiData.data))
   .catch(error => console.log(error))
 }
 getApiData();
 
-// console.log(apiData);
-function startGame(data) {
+const startGame = (data) => {
   game = new Game(data);
-  game.findSurveys();
-  console.log(game);
+  // game.findSurveys();
 }
+
+$(".start-btn").click(() => {
+  game.getPlayers($(".plyr-input:eq(0)").val(), $(".plyr-input:eq(1)").val());
+  domUpdates.checkInput()
+})
+
+const doThing = e => {
+  if($(e.target).hasClass("info-btn")) {
+  $(".player1-score").text(game.player1.score)
+  }
+}
+
+$(".info-btn").click(domUpdates.openInfo);
+$(".close-btn").click(domUpdates.closeInfo)
