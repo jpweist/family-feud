6baf1a17e90e66ea90b548a35d9a3950199a47e9
Jim Weist <jp_weist@yahoo.com>
2020-01-08 05:59:40 -0700

    Removed unused test from turn-test

--- test/turn-test.js
+++ test/turn-test.js
@@ -22,32 +22,4 @@ describe('Turn Class', function() {
     expect(turn1.answers).to.deep.equal({ answer: 'Alarm Clock', respondents: 34, surveyId: 3 });
   });
 
-  it.skip('should have a method to check answer', function() {
-    expect(true).to.equal(false);
-  });
-
-  it.skip('should have a method to give feed back', function() {
-    expect(true).to.equal(false);
-  });
-
-  it.skip('should have a method to increase the score', function() {
-    expect(true).to.equal(false);
-  });
-
-  it.skip('should have a method to start fast money round', function() {
-    expect(true).to.equal(false);
-  });
-
-  it.skip('should have a method to for a timer', function() {
-    expect(true).to.equal(false);
-  });
-
-  it.skip('should have a method for the multipler', function() {
-    expect(true).to.equal(false);
-  });
-
-  it.skip('should have a method to start game', function() {
-    expect(true).to.equal(false);
-  });
-
 });
